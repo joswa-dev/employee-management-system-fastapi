7d00edfb8c013cba1e90d0a3509959c52221eeb8
Shibin Joswa <shibinjoswa@gmail.com>
2026-06-09 13:24:31 +0530

    Added admin startup code

--- main.py
+++ main.py
@@ -2,8 +2,32 @@ from fastapi import FastAPI
 
 from app.database import engine, Base
 from app.routes import router
+from app.database import SessionLocal
+from app.models import Employee
+from app.auth import hash_password # or wherever your hash function is
 
 Base.metadata.create_all(bind=engine)
+
+db = SessionLocal()
+
+admin = db.query(Employee).filter(
+    Employee.email == "admin@gmail.com"
+).first()
+
+if not admin:
+    admin_user = Employee(
+        name="admin1",
+        email="admin@gmail.com",
+        password=hash_password("admin123"),
+        position="Developer",
+        department="IT",
+        role="admin"
+    )
+
+    db.add(admin_user)
+    db.commit()
+
+db.close()
 
 app = FastAPI(
     title="Employee Management System"
